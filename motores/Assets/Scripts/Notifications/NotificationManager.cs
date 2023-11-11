3bcd4a5f3af86dc5e361f79a2a9ecf36fe491ca5
loupancas <129621626+loupancas@users.noreply.github.com>
2023-11-11 11:42:01 -0500

    NotificationManager

---
+++ motores/Assets/Scripts/Notifications/NotificationManager.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using Unity.Notifications.Android;
+public class NotificationManager : MonoBehaviour
+{
+    public static NotificationManager Instance 
+    {
+        get; private set;
+    }
+
+    AndroidNotificationChannel notifChannel;
+    private void Awake()
+    {
+        if(Instance != this && Instance!=null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        AndroidNotificationCenter.CancelAllDisplayedNotifications();
+        AndroidNotificationCenter.CancelAllScheduledNotifications();
+        notifChannel = new AndroidNotificationChannel()
+        {
+            Id = "reminder_notif_ch",
+            Name = "Reminder Notification",
+            Description = "Reminder to login",
+            Importance = Importance.High
+        };
+        AndroidNotificationCenter.RegisterNotificationChannel(notifChannel);
+        DisplayNotification("Vuelve", "Juega otra vez", 
+            IconSelect.icon_reminder, IconSelect.icon_reminderBig, DateTime.Now.AddHours(24));
+    }
+    public int DisplayNotification(string title, string text, IconSelect iconSmall, IconSelect iconLarge, DateTime fireTime)
+    {
+        var notification = new AndroidNotification();
+        notification.Title = title;
+        notification.Text = text;
+        notification.SmallIcon = iconSmall.ToString();
+        notification.LargeIcon = iconLarge.ToString();
+        notification.FireTime = fireTime;
+
+        return AndroidNotificationCenter.SendNotification(notification, notifChannel.Id);
+
+
+    }
+
+    public void CancelNotification(int id)
+    {
+        AndroidNotificationCenter.CancelScheduledNotification(id);
+    }
+
+    public enum IconSelect
+    {
+        icon_reminder,
+        icon_reminderBig
+
+    }
+}
--- motores/Assets/Scripts/StaminaSystem/StaminaSystem.cs
+++ motores/Assets/Scripts/StaminaSystem/StaminaSystem.cs
@@ -8,7 +8,7 @@ public class StaminaSystem : MonoBehaviour
 {
     [SerializeField] int _maxStamina = 10;
     [SerializeField] float _timeToRecharge = 10f;
-    int currentStamina;
+    int _currentStamina;
 
     DateTime _nextStaminaTime;
     DateTime _lastStaminaTime;
@@ -21,14 +21,118 @@ public class StaminaSystem : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        LoadData();
+        StartCoroutine(RechargeStamina());
+
     }
 
-    // Update is called once per frame
-    void Update()
+
+    IEnumerator RechargeStamina()
     {
-        
+        UpdateTimer();
+        UpdateStamina();
+        recharging = true;
+
+        while(_currentStamina<_maxStamina)
+        {
+            DateTime current = DateTime.Now;
+            DateTime nexTime = _nextStaminaTime;
+            // bool chequea se recargo stamina
+
+            bool addingStamina = false;
+            while(current > nexTime)
+            {
+                if (_currentStamina >= _maxStamina) break;
+                _currentStamina += 1;
+                addingStamina = true;
+                UpdateStamina();
+
+                // predecir prox ves que se va a recargar la stamina
+                DateTime timeToAdd = nexTime;
+                // chequear si el usuario cerro la app
+
+                if(_lastStaminaTime>nexTime)
+                {
+                    timeToAdd = _lastStaminaTime;
+                }
+                nexTime = AddDuration(timeToAdd, _timeToRecharge);
+
+            }    
+            // si se recargo stamina
+            if(addingStamina)
+            {
+                _nextStaminaTime = nexTime;
+                _lastStaminaTime = DateTime.Now;
+            }
+
+            //update ui 
+            SaveInformation();
+            UpdateStamina();
+            UpdateTimer();
+
+            yield return new WaitForEndOfFrame();
+        }
+        recharging = false;
     }
+
+    private DateTime AddDuration(DateTime timeToAdd, float timeToRecharge)
+    {
+        return timeToAdd.AddSeconds(timeToRecharge);
+        //return timeToAdd.AddMinutes(timeToRecharge);
+
+    }
+
+    public bool HasEnoughStamina(int stamina)
+    {
+        return _currentStamina - stamina >= 0;
+    }
+
+    public void UseStamina( int staminaToUse)
+    {
+        if(_currentStamina - staminaToUse >= 0)
+        {
+            //jugar nivel
+            _currentStamina -= staminaToUse;
+            UpdateStamina();
+            if(!recharging)
+            {
+                // setear next stamina time y comenzar recarga
+                _nextStaminaTime = AddDuration(DateTime.Now, _timeToRecharge);
+                StartCoroutine(RechargeStamina());
+            }
+        }
+        else
+        {
+            Debug.Log("Sin stamina suficiente");
+        }
+    }
+
+
+    private void UpdateStamina()
+    {
+        _staminaText.text = $"{_currentStamina}/{_maxStamina}";
+        //_staminaText.text = _currentStamina.ToString("");
+    }
+
+    private void UpdateTimer()
+    {
+        if (_currentStamina >= _maxStamina)
+        {
+            _timerText.text = "Full stamina";
+            return;
+
+        }
+        //estructura que de un intervalo de tiempo 
+        TimeSpan timer = _nextStaminaTime - DateTime.Now;
+
+        //formato ceros para representar horario como su fuese reloj
+
+        _timerText.text = timer.Minutes.ToString("00") + ":" + timer.Seconds.ToString("00");
+
+
+    }
+
+
     void SaveInformation()
     {
        
@@ -36,7 +140,7 @@ public class StaminaSystem : MonoBehaviour
         //next Stamina time
         //last satimana time
 
-        //PlayerPrefs.SetInt(SaveData.currentStaminaKey,_currentStamina);
+        PlayerPrefs.SetInt(SaveData.currentStaminaKey,_currentStamina);
         PlayerPrefs.SetString(SaveData.nextStaminaTimeKey, _nextStaminaTime.ToString());
         PlayerPrefs.SetString(SaveData.lastStaminaTimeKey, _lastStaminaTime.ToString());
 
@@ -45,7 +149,7 @@ public class StaminaSystem : MonoBehaviour
 
     void LoadData()
     {
-        //_currentStamina = PlayerPrefs.GetInt(SaveData.currentStaminaKey);
+        _currentStamina = PlayerPrefs.GetInt(SaveData.currentStaminaKey, _maxStamina);
 
         //_nextStaminaTime = DateTime.Parse(PlayerPrefs.GetString(SaveData.nextStaminaTimeKey));
         //_lastStaminaTime = DateTime.Parse(PlayerPrefs.GetString(SaveData.lastStaminaTimeKey));
